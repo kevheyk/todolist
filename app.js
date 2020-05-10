374e8622d7a888e1bf8efb614b1f3df2668400b2
Kevin He <kevheyk@gmail.com>
2020-05-09 20:00:13 -0600

    Complete work listing with button value directing. With optional postAction directing.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.listen(3000, function () {
 });
 // Global variable declaration:
 let day = "";
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+let dailyItems = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 
 
@@ -25,22 +26,58 @@ day = new Date().toLocaleString("en-US", {
   month: "long",
 });
 console.log(day);
+
 // Home Route Logic
 app.get("/", function (req, res) {
   // Date class use system date and default input.
-
   res.render("list", {
+    kindOfList: "Daily List",
     kindOfDay: day,
-    newListItem: items,
-    items:items
+    items:dailyItems,
+    // Optional: Use postAction to differentiate which post to render
+    postAction: "/"
     
   });
 });
 
-// Receive data from List
-app.post("/", function (req, res) {
+// Receive data from Daily List
+app.get("/work", function (req, res) {
   
-  items.push(req.body.newItem);
-  res.redirect("/");
+  res.render("list", {
+    kindOfList: "Work List",
+    kindOfDay: day,
+    items:workItems,
+    // Optional: Set this one to "/work" to activate postAction directing.
+    postAction: "/"
+  });
   
 });
+
+
+// Receive data from Daily List
+app.post("/", function (req, res) {
+  // Use button value to direct which list to GET
+  switch(req.body.kindOfList){
+    case "Daily List":
+      dailyItems.push(req.body.newItem);
+      res.redirect("/");
+      break;
+    case "Work List":
+      workItems.push(req.body.newItem);
+      res.redirect("/work")
+      break;
+    default:
+      console.log("What? This new item is from somewhere else!"+req.body.kindOfList+".")
+  }
+    
+  
+});
+// Optional. Not working as I have set postAction always "/".
+// Receive data from Work List
+app.post("/work", function (req, res) {
+  console.log(req.body);
+  workItems.push(req.body.newItem);
+  res.redirect("/work");
+
+});
+
